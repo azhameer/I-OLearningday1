e45120fa5c36b061ec78e5f0317400070f54ec04
Zameer <azhameer13@gmail.com>
2026-09-20 14:40:11 +0700

    Calculate circumference instead of area of circle

--- circumference.py
+++ circumference.py
@@ -1,4 +1,4 @@
 pi = 3.14
 jariJari = float(input("Masukkan jari-jari lingkaran: "))
-luasLingkaran = pi * jariJari ** 2
-print("Luas lingkaran dengan jari-jari", jariJari, "adalah", luasLingkaran)
+kelilingLingkaran = 2 * pi * jariJari
+print("Keliling lingkaran dengan jari-jari", jariJari, "adalah", kelilingLingkaran)
