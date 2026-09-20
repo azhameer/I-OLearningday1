12adcbcda6986f8def9940c8742e0f7ac2b1852d
Zameer <azhameer13@gmail.com>
2026-09-20 14:34:57 +0700

    Add user input and output for name, age, and height

---
+++ IObio-Data.py
@@ -0,0 +1,9 @@
+#Input User
+nama = input ("Masukkan Nama Anda: ")
+umur = int(input("Masukkan Umur Anda: ")
+tinggi = float(input("Masukkan Tinggi Badan Anda: ")
+
+#Output user
+print(f"Nama: {nama}")
+print(f"Umur: {umur} tahun")
+print(f"Tinggi: {tinggi} meter")
